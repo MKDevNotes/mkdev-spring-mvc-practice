09102b38b2cdd2c2cfafd61f88b43256d523bb1e
Muthukumar Thangavinayagam <2063172+mkpracticalworks@users.noreply.github.com>
2019-09-01 12:06:50 +0530

    Servlet API version updated

--- dn-jetty-simple-webapp/src/main/java/com/dn/simplejettyweb/servlet/DnSimpleJettyServlet.java
+++ dn-jetty-simple-webapp/src/main/java/com/dn/simplejettyweb/servlet/DnSimpleJettyServlet.java
@@ -4,22 +4,28 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 /***
  * @author Muthukumar Thangavinayagam.
  */
+@WebServlet("/dnsimplejetty")
 public class DnSimpleJettyServlet extends HttpServlet{
 
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("Servlet Called..");
-        PrintWriter out = resp.getWriter();
-        out.println("Hello Dn Simple Jetty Servlet");
-        out.flush();
-        out.close();
-        super.doGet(req, resp);
+        doPost(req, resp);
     }
 
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+                    out.println("Hello Dn Simple Jetty Servlet");
+                    out.flush();
+                    out.close();
+        super.doPost(req, resp);
+    }
 }
